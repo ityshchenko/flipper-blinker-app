19953bb6f1b3866934eff37e2858e18033692aa2
Ivan Tyshchenko <5057658+nohitmai@users.noreply.github.com>
2022-11-22 14:12:55 +0000

    Update blinker

--- blinker.c
+++ blinker.c
@@ -1,19 +1,46 @@
 #include <furi.h>
+#include <furi_hal.h>
+
 #include <gui/gui.h>
 #include <input/input.h>
 #include <stdlib.h>
+#include <time.h>
 
-int32_t blinker_app() { 
-    // osMessageQueueId_t event_queue = osMessageQueueNew(8, sizeof(GameEvent), NULL); 
- 
-    // Set system callbacks
-    // ViewPort* view_port = view_port_alloc(); 
-    // view_port_draw_callback_set(view_port, render_callback, NULL);
-    // view_port_input_callback_set(view_port, input_callback, event_queue);
- 
-    // Open GUI and register view_port
-    // Gui* gui = furi_record_open("gui"); 
-    // gui_add_view_port(gui, view_port, GuiLayerFullscreen); 
+// Screen is 128x64 px
+static void app_draw_callback(Canvas* canvas, void* ctx) {
+    UNUSED(ctx);
 
+    canvas_clear(canvas);
+    // canvas_draw_icon(canvas, image_position.x % 128, image_position.y % 64, &I_dolphin_71x25);
+}
+
+static void app_input_callback(InputEvent* input_event, void* ctx) {
+    furi_assert(ctx);
+
+    FuriMessageQueue* event_queue = ctx;
+    furi_message_queue_put(event_queue, input_event, FuriWaitForever);
+}
+
+int32_t blinker_app(void *p) { 
+    UNUSED(p);
+
+    FuriMessageQueue* event_queue = furi_message_queue_alloc(8, sizeof(InputEvent));
+
+    // Configure view port
+    ViewPort* view_port = view_port_alloc();
+    view_port_draw_callback_set(view_port, app_draw_callback, view_port);
+    view_port_input_callback_set(view_port, app_input_callback, event_queue);
+    
+    Gui* gui = furi_record_open(RECORD_GUI);
+    gui_add_view_port(gui, view_port, GuiLayerFullscreen);
+
+    time.sleep(1)
+
+    view_port_enabled_set(view_port, false);
+    gui_remove_view_port(gui, view_port);
+    view_port_free(view_port);
+    furi_message_queue_free(event_queue);
+
+    furi_record_close(RECORD_GUI);
     return 0;
 }
