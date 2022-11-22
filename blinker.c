55195c6fdc454f00a2daac1d0110680a0878f3bf
Ivan Tyshchenko <5057658+nohitmai@users.noreply.github.com>
2022-11-22 13:48:59 +0000

    Add empty app

---
+++ blinker.c
@@ -0,0 +1,19 @@
+#include <furi.h>
+#include <gui/gui.h>
+#include <input/input.h>
+#include <stdlib.h>
+
+int32_t blinker_app() { 
+    // osMessageQueueId_t event_queue = osMessageQueueNew(8, sizeof(GameEvent), NULL); 
+ 
+    // Set system callbacks
+    // ViewPort* view_port = view_port_alloc(); 
+    // view_port_draw_callback_set(view_port, render_callback, NULL);
+    // view_port_input_callback_set(view_port, input_callback, event_queue);
+ 
+    // Open GUI and register view_port
+    // Gui* gui = furi_record_open("gui"); 
+    // gui_add_view_port(gui, view_port, GuiLayerFullscreen); 
+
+    return 0;
+}
